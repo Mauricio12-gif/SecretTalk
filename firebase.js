52935ab5d540b3a3b8d781685b157809859f3ecb
Mauricio12-gif <lovermax876@gmail.com>
2026-07-31 20:57:42 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,36 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 
+
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyDWoOntz_Xgz1nxgSwjg9M86WmtJfn9Bmo",
+
+authDomain: "secrettalk-d6fe6.firebaseapp.com",
+
+projectId: "secrettalk-d6fe6",
+
+storageBucket: "secrettalk-d6fe6.firebasestorage.app",
+
+messagingSenderId: "273110228571",
+
+appId: "1:273110228571:web:5ffc4fa713c4b6fc9fa97e"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+
+const db = getFirestore(app);
+
+
+
+export { db };
