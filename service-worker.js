895383ad52e10d81922cb5862737d48d69a3a453
Mauricio12-gif <lovermax876@gmail.com>
2026-08-02 00:20:20 +0300

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,40 @@
-const CACHE_NAME = "secrettalk-v1";
+const CACHE_NAME = "secrettalk-v2";
+
 
 const FILES_TO_CACHE = [
-  "/SecretTalk/",
-  "/SecretTalk/index.html",
-  "/SecretTalk/style.css",
-  "/SecretTalk/manifest.json"
+
+"/SecretTalk/",
+
+"/SecretTalk/index.html",
+
+"/SecretTalk/login.html",
+
+"/SecretTalk/signup.html",
+
+"/SecretTalk/receiver.html",
+
+"/SecretTalk/chat.html",
+
+"/SecretTalk/style.css",
+
+"/SecretTalk/manifest.json",
+
+"/SecretTalk/icon.png"
+
 ];
 
 
+
+
+// INSTALL
+
 self.addEventListener("install", (event)=>{
+
 
 event.waitUntil(
 
 caches.open(CACHE_NAME)
+
 .then((cache)=>{
 
 return cache.addAll(FILES_TO_CACHE);
@@ -21,19 +43,72 @@ return cache.addAll(FILES_TO_CACHE);
 
 );
 
+
+self.skipWaiting();
+
+
 });
 
 
 
-self.addEventListener("fetch", (event)=>{
+
+
+// ACTIVATE
+
+self.addEventListener("activate",(event)=>{
+
+
+event.waitUntil(
+
+caches.keys().then((keys)=>{
+
+
+return Promise.all(
+
+keys.map((key)=>{
+
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+
+})
+
+);
+
+
+})
+
+);
+
+
+self.clients.claim();
+
+
+});
+
+
+
+
+
+
+// FETCH
+
+self.addEventListener("fetch",(event)=>{
 
 
 event.respondWith(
 
 caches.match(event.request)
+
 .then((response)=>{
 
+
 return response || fetch(event.request);
+
 
 })
 
