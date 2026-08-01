7827ac600a6ad764b806883cea0f61e463e94423
Mauricio12-gif <lovermax876@gmail.com>
2026-08-02 02:13:58 +0300

    Refactor login function to use username and password

--- login.js
+++ login.js
@@ -1,52 +1,129 @@
-import { auth } from "./firebase.js";
+import { db } from "./firebase.js";
+
 
 import {
-signInWithEmailAndPassword
+collection,
+query,
+where,
+getDocs
 }
 from
-"https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 
-async function login(){
 
-const email =
-document.getElementById("email").value.trim();
+window.login = async function(){
+
+
+const username =
+document.getElementById("username")
+.value
+.trim();
+
 
 const password =
-document.getElementById("password").value.trim();
+document.getElementById("password")
+.value
+.trim();
+
 
 const status =
 document.getElementById("status");
 
 
+
+if(username === "" || password === ""){
+
+
+status.innerHTML =
+"Fill all fields";
+
+
+return;
+
+}
+
+
+
 try{
 
-await signInWithEmailAndPassword(
-auth,
-email,
-password
+
+const usersQuery = query(
+
+collection(db,"users"),
+
+where("username","==",username),
+
+where("password","==",password)
+
 );
+
+
+
+const result = await getDocs(usersQuery);
+
+
+
+if(result.empty){
+
+
+status.innerHTML =
+"Wrong username or password ❌";
+
+
+return;
+
+
+}
+
+
+
+const userDoc =
+result.docs[0];
+
+
+const userData =
+userDoc.data();
+
+
+
+localStorage.setItem(
+"username",
+userData.username
+);
+
+
 
 status.innerHTML =
 "Login successful ❤️";
 
+
+
+setTimeout(()=>{
+
+
 window.location.href =
 "receiver.html";
 
+
+},1000);
+
+
+
 }
 
+
 catch(error){
+
+
+console.log(error);
+
 
 status.innerHTML =
 error.message;
 
-console.log(error);
 
 }
 
-}
 
-
-document
-.getElementById("loginButton")
-.addEventListener("click", login);
+};
