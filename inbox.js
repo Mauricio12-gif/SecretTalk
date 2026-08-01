cfb1a392e14493acb9f23d119d93bc580634b5b4
Mauricio12-gif <lovermax876@gmail.com>
2026-08-01 10:13:11 +0300

    Refactor inbox.js to improve message rendering

--- inbox.js
+++ inbox.js
@@ -8,85 +8,48 @@ onSnapshot,
 
 query,
 
-orderBy,
-
-where
+orderBy
 
 } from
 "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
-
-
 const messagesDiv = document.getElementById("messages");
-
-
-
-const params = new URLSearchParams(window.location.search);
-
-const username = params.get("user") || "Mauricio";
-
-
 
 const messagesQuery = query(
 
 collection(db,"messages"),
 
-where("receiver","==",username),
-
 orderBy("time","desc")
 
 );
 
-
-
 onSnapshot(messagesQuery,(snapshot)=>{
 
-
-messagesDiv.innerHTML =
-
-"<h2>Inbox for " + username + "</h2>";
-
-
+messagesDiv.innerHTML = "";
 
 if(snapshot.empty){
 
-messagesDiv.innerHTML +=
-
-"<p>No messages yet</p>";
+messagesDiv.innerHTML =
+"No messages yet";
 
 return;
 
 }
 
-
-
 snapshot.forEach((doc)=>{
-
 
 const data = doc.data();
 
-
-
 messagesDiv.innerHTML += `
 
-<div style="
-background:#ffeef5;
-padding:15px;
-margin:10px;
-border-radius:15px;
-">
+<div style="  
+background:#ffeef5;  
+padding:15px;  
+margin:10px;  
+border-radius:15px;  
+">  <p>${data.text}</p>  <small>Anonymous</small>
 
-<p>${data.text}</p>
-
-<small>Anonymous</small>
-
-</div>
-
-`;
-
-
+</div>  `;
 
 });
-
-
-});
+  
