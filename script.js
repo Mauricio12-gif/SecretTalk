129b38b3977cae351dddf0cb4c4355ff57ad3774
Mauricio12-gif <lovermax876@gmail.com>
2026-08-01 11:47:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -90,3 +90,57 @@ status.innerHTML =
 
 
 };
+// CREATE SECRET TALK LINK
+
+window.createLink = function(){
+
+
+const username = document
+.getElementById("username")
+.value
+.trim();
+
+
+
+const result = document
+.getElementById("result");
+
+
+
+if(username === ""){
+
+
+result.innerHTML =
+"Enter a username first";
+
+return;
+
+}
+
+
+
+const link =
+
+window.location.origin +
+
+"/SecretTalk/?user=" +
+
+encodeURIComponent(username);
+
+
+
+result.innerHTML = `
+
+Your SecretTalk link:
+
+<br><br>
+
+<a href="${link}">
+${link}
+</a>
+
+`;
+
+
+
+};
